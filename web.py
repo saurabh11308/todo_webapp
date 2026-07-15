afd7e46d94999df82d7f8c326ac6a527f9b3c430
Saurabh <saurabh11308@yahoo.com>
2026-07-15 23:13:31 +0530

    Implemented the add todo function

--- web.py
+++ web.py
@@ -1,15 +1,25 @@
 import streamlit as st
 from backend import get_todos,set_todos
 
+todos = get_todos()
+
+def add_todo():
+    todo_new = st.session_state["new_todo"] + "\n"
+    todos.append(todo_new)
+    set_todos(todos)
+
 st.title("Welcome to TODO app")
 st.subheader("This is a todo app which tracks changes")
 st.write("The purpose of this app is to improve productivity")
 
-todos = get_todos()
+for index,todo in enumerate(todos):
+    st.checkbox(todo,key=index)
+    if st.session_state[index] == True:
+        print("Hadippa")
 
-for todo in todos:
-    st.checkbox(todo)
-
-st.text_input(label="",placeholder="Add a todo here")
+st.text_input(label="",placeholder="Add a todo here",on_change=add_todo,
+              key="new_todo")
 
 print("Hello")
+
+st.session_state
